28e5cf6dcc6eb20a09bf457733ced9d8e8d24cc6
Parshuram Kumar <learnerparshuram@gmail.com>
2025-08-12 20:01:24 +0530

    Time: 4 ms (34.94%) | Memory: 17.8 MB (80.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -9,13 +9,16 @@ public:
 
         priority_queue<pair<int,int> , vector<pair<int,int>>, function<bool(pair<int,int>, pair<int,int>)>> pq(
             [](pair<int,int> a, pair<int, int>b){
-                return a.second < b.second;
+                return a.second > b.second;
             }
         );
 
         for(auto a : mp){
             pair<int,int> p = {a.first, a.second};
             pq.push(p);
+            if(pq.size() > k){
+                pq.pop();
+            }
         }
         vector<int> ans;
         while(k--){
