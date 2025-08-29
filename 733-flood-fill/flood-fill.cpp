efca271d324d651d53cd563578dfb0b8b1239815
Parshuram Kumar <learnerparshuram@gmail.com>
2025-08-29 13:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (60.03%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -3,8 +3,9 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
         int in = image[sr][sc];
-        // if(in == color) return image;
-        dfs(image, sr, sc, color, in);
+        if(in == color) return image;
+        // dfs(image, sr, sc, color, in);
+        bfs(image, sr, sc, color, in);
         return image;
 
     }
@@ -30,4 +31,33 @@ public:
         dfs(image, sr, sc-1, color, in);
         
     }
+
+    void bfs(vector<vector<int>>& image, int sr, int sc, int color, int ini){
+        queue<pair<int, int>> q;
+        q.push({sr,sc});
+        image[sr][sc] = color;
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            if(row+1 < image.size() && image[row+1][col] == ini){
+                q.push({row+1,col});
+                image[row+1][col] = color;
+            }
+            if(row - 1 >= 0 && image[row-1][col] == ini){
+                 q.push({row-1,col});
+                 image[row-1][col] = color;
+            }
+            if(col + 1 < image[0].size() && image[row][col+1] == ini){
+                 q.push({row,col+1});
+                 image[row][col+1] = color;
+            }
+            if(col - 1 >= 0 && image[row][col-1] == ini){
+                 q.push({row,col-1});
+                 image[row][col-1] = color;
+            }
+        }
+    }
+
 };
