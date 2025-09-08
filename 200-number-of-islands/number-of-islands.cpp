afde1e5462561763523388b3840e180f44907ecf
Parshuram Kumar <learnerparshuram@gmail.com>
2025-09-08 19:43:27 +0530

    Time: 31 ms (32.16%) | Memory: 23.8 MB (28.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,70 +1,52 @@
 class Solution {
 public:
-    int m;
-    int n;
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        
+        int m = grid.size();
+        int n = grid[0].size();
         int cnt = 0;
-        vector<vector<bool>> v(m,vector<bool>(n,false));
-        for(int i = 0; i < m ; ++i){
+        vector<vector<bool>> vis(m, vector<bool>(n, true));
+        for(int i = 0; i < m; ++i){ 
             for(int j = 0; j < n; ++j){
-                if(v[i][j] == false && grid[i][j] != '0'){
+                if(grid[i][j] == '1' && vis[i][j] == true){
                     cnt++;
-                    // dfs(i , j, grid,v);
-                    bfs(i, j, grid, v);
+                    bfs(i, j, grid,vis);
                 }
-                
             }
         }
-        return cnt;
+        return cnt;    
     }
-    void dfs(int row, int col,vector<vector<char>>& grid , vector<vector<bool>> &v){
-        if(row < 0 || row >= m || col < 0 || col >= n){
-            return;
-        }
-
-        if(v[row][col] == true || grid[row][col] == '0'){
-            return;
-        }
-
-        v[row][col] = true;
-
-        dfs(row + 1, col, grid, v);
-        dfs(row -1 , col , grid, v);
-        dfs(row, col + 1, grid,v);
-        dfs(row, col-1, grid,v);
-
-    }
-
-    void bfs(int row, int col, vector<vector<char>>& grid , vector<vector<bool>> &v){
+    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<bool>> &vis){
+        int n = grid.size();
+        int m = grid[0].size();
         queue<pair<int, int>> q;
         q.push({row, col});
-        v[row][col] = true;
-       
-       while(!q.empty()){
+        vis[row][col] = false;
+        while(!q.empty()){
             int r = q.front().first;
             int c = q.front().second;
             q.pop();
-
-            if(r+1 < m && v[r+1][c] == false && grid[r+1][c] != '0'){
+            //down checking
+            if(r+1 < n && vis[r+1][c] == true && grid[r+1][c] == '1' ){
                 q.push({r+1, c});
-                v[r+1][c] = true;
+                vis[r+1][c] = false;
             }
-            if(r-1 >= 0 && v[r-1][c] == false && grid[r-1][c] != '0'){
-                q.push({r-1, c});
-                v[r-1][c] = true;
-            }
-            if(c+1 < n && v[r][c+1] == false && grid[r][c+1] != '0'){
+            //right checking
+            if(c+1 < m && vis[r][c+1] == true && grid[r][c+1] == '1' ){
                 q.push({r, c+1});
-                v[r][c+1] = true;
+                vis[r][c+1] = false;
             }
-            if(c-1 >= 0 && v[r][c-1] == false && grid[r][c-1] != '0'){
-                q.push({r, c-1});
-                v[r][c-1] = true;
+            //up checking
+             if(r-1 >= 0 && vis[r-1][c] == true && grid[r-1][c] == '1' ){
+                q.push({r-1, c});
+                vis[r-1][c] = false;
             }
 
-       }
-        
+            //left checking
+              if(c-1 >= 0 && vis[r][c-1] == true && grid[r][c-1] == '1' ){
+                q.push({r, c-1});
+                vis[r][c-1] = false;
+            }
+        }
     }
 };
