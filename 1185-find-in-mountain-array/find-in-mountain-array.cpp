80b3bfc76360aa8b05ed179ac8a43ba7c7cff30e
Parshuram Kumar <learnerparshuram@gmail.com>
2025-09-18 15:36:03 +0530

    Time: 8 ms (21.09%) | Memory: 9.9 MB (69.07%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.cpp
@@ -0,0 +1,62 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length();
+        int start = 0;
+        int end = n-1;
+        int peak = - 1;
+        while(start <= end){
+            int mid = start + ((end -start) /2);
+            if(mid!=0 && mid != n-1 && mountainArr.get(mid-1) < mountainArr.get(mid) && mountainArr.get(mid) > mountainArr.get(mid + 1)){
+                peak =  mid;
+                break;
+            }
+            else if (mid != n-1 && mountainArr.get(mid) < mountainArr.get(mid+1)){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+
+        }
+
+        int a = bs(mountainArr, target, 0 , peak, true);
+        int b =  bs(mountainArr, target, peak + 1, n-1 , false );
+
+        return a == -1 && b == -1 ? -1 : a == -1 ? b : a;
+        
+    }
+
+    int bs(MountainArray &mountainArr, int target, int start, int end, bool asc){
+        
+        while(start <= end){
+            int mid = start + ((end - start) / 2);
+            int midVal = mountainArr.get(mid);
+            if(midVal == target){
+                return mid;
+            }
+            else if (midVal < target){
+                if(asc)
+                    start = mid + 1;
+                else end = mid -1;
+            }
+            else{
+                if(asc) end = mid - 1;
+                else start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    
+};
