584b726edc63ba6069f1255fc69bdca3b4451e11
Parshuram Kumar <learnerparshuram@gmail.com>
2025-09-24 15:21:21 +0530

    Time: 80 ms (31.61%) | Memory: 53.9 MB (7.33%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,18 +4,16 @@ public:
         int cnt = 0;
         int n = nums.size();
         map<int, int> mp;
-        mp[0]  = 1;
+        mp[0] = 1;
         int sum = 0;
-        for(int i = 0; i < n; ++i){
+        for (int i = 0; i < n; ++i) {
             sum += nums[i];
 
-            if(mp.contains(sum - k)){
-                cnt += mp[sum - k];
-            }
+            cnt += mp[sum - k];
 
-           mp[sum]++;
+            mp[sum]++;
         }
-        
+
         return cnt;
     }
 };
