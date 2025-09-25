01390a549476012c3f7b388bbf14b0f0344dda32
Parshuram Kumar <learnerparshuram@gmail.com>
2025-09-25 18:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (8.23%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    // int solveDP(vector<vector<int>> tri, int start, int ind, int n,
-    // vector<vector<int>> &dp){
-    //     if(start == n-1){
-    //         return dp[start][ind] = tri[start][ind];
-    //     }
-    //     if(dp[start][ind] != -1) return dp[start][ind];
-    //     int a = tri[start][ind] + solveDP(tri, start+1, ind, n, dp );
-    //     int b = tri[start][ind] + solveDP(tri, start + 1, ind + 1, n, dp);
-
-    //     return dp[start][ind] = min(a, b);
-    // }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        // return solveDP(triangle, 0,0, n, dp);
-
-        for (int i = n - 1; i >= 0; i--) {
-            dp[n - 1][i] = triangle[n - 1][i];
+        int m = triangle[n-1].size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int j = 0; j < n; ++j){
+            dp[n-1][j] = triangle[n-1][j];
         }
+        for(int i = n-2; i >= 0 ; --i){
+            for(int j = i; j >= 0; --j){
+                int down = dp[i + 1][j];
+                int diag = dp[i+1][j+1];
 
-        for (int i = n - 2; i >= 0; --i) {
-            for (int j = i; j >= 0; --j) {
-                int a = triangle[i][j] + dp[i + 1][j];
-                int b = triangle[i][j] + dp[i + 1][j + 1];
-
-                dp[i][j] = min(a, b);
+                dp[i][j] = triangle[i][j] + min(down, diag);
             }
         }
         return dp[0][0];
+    }
+
+    int solve(int i, int j, vector<vector<int>> &triangle, vector<vector<int>> &dp){
+        if(i == triangle.size()-1){
+            return triangle[i][j];
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int cur = triangle[i][j];
+        int a = solve(i + 1, j, triangle, dp);
+        int b = solve(i + 1, j + 1, triangle, dp);
+
+        return dp[i][j] = cur + min(a, b);
     }
 };
