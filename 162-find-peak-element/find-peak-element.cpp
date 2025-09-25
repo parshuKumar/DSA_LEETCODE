c71f5e040a93bb7d0eec69b95592ed6e66dbc9ba
Parshuram Kumar <learnerparshuram@gmail.com>
2025-09-25 09:39:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (92.39%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -13,11 +13,11 @@ public:
         }
 
         // if slope is rising, move right
-        if (mid < n-1 && arr[mid] < arr[mid+1]) {
-            start = mid + 1;
+        if (mid > 0 &&  arr[mid] < arr[mid-1]) {
+            end = mid - 1;
         } 
         else {
-            end = mid - 1;
+            start = mid + 1;
         }
     }
         return -1;
