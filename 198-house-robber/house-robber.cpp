1bef49919a33188709ad66daee01e2b9e234d656
Parshuram Kumar <learnerparshuram@gmail.com>
2025-11-08 16:11:03 +0530

    Time: 3 ms (6.18%) | Memory: 11.6 MB (5.80%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        // vector<int> dp(n + 1, -1);
-        // return solve(0, nums, dp);
-        int prev = 0;
-        int prev1 = nums[n-1];
-        for(int i = n-2; i >= 0; --i){
-           int pick = nums[i] + prev;
-           int notPick = prev1;
-           
-           int ans = max(pick, notPick);
-
-           prev = prev1;
-           prev1 = ans; 
-
-        }
-        return prev1;
+        vector<vector<int>> dp(nums.size() + 1, vector<int> (nums.size() + 1, -1));
+        return solve(0, -1, nums, dp);
     }
 
-    // int solve(int start, vector<int> &nums, vector<int> &dp){
-    //     if( start >= nums.size()){
-    //         return 0;
-    //     }
-    //     if(dp[start] != -1) return dp[start];
+    int solve(int cur, int prev, vector<int> &nums,vector<vector<int>> &dp){
+        if(cur >= nums.size()){
+            return 0;
+        }
+        if(dp[prev + 1][cur] != -1) return dp[prev + 1][cur];
+        int take = 0;
+        if(prev == -1 || cur - prev != 1) take = nums[cur] + solve(cur + 1, cur, nums, dp);
+        int nottake = solve(cur + 1, prev, nums, dp);
 
-    //     int pick = nums[start] + solve(start + 2, nums, dp);
-    //     int notPick = solve(start +1, nums, dp);
-
-    //     return dp[start] = max(pick, notPick);
-    // }
+        return dp[prev + 1][cur] = max(take, nottake);
+    }
 };
