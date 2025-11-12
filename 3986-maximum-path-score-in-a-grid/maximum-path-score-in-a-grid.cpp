d88b374e5c45a98b7454ff4532215c916e5b5abb
Parshuram Kumar <learnerparshuram@gmail.com>
2025-11-12 13:16:23 +0530

    Time: 952 ms (30.74%) | Memory: 528.4 MB (8.40%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -3,7 +3,7 @@ public:
     int maxPathScore(vector<vector<int>>& grid, int k) {
         
         
-        vector<vector<vector<int>>> dp(grid.size() + 1, vector<vector<int>> (grid[0].size() + 1, vector<int>(k + 2, -1)));
+        vector<vector<vector<int>>> dp(grid.size() + 1, vector<vector<int>> (grid[0].size() + 1, vector<int>(k + 1, -1)));
         int ans =  solve(grid, 0, 0, k, dp);
         return (ans < 0 ? -1 : ans);
     }
@@ -24,13 +24,13 @@ public:
             return grid[row][col];
         }
 
-        if(dp[row][col][k + 1] != -1) return dp[row][col][k + 1];
+        if(dp[row][col][k] != -1) return dp[row][col][k];
 
         int down = solve(grid, row + 1, col, k, dp);
         int right = solve(grid, row, col + 1, k, dp);
 
         int best = max(down, right);
-        return dp[row][col][k +1] = (best == -1e9 ? -1e9 : grid[row][col] + best); 
+        return dp[row][col][k] = (best == -1e9 ? -1e9 : grid[row][col] + best); 
     }
 
 
