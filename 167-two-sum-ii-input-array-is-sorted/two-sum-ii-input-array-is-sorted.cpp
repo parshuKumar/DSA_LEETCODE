b17272e4595067fff988729c353abdc9c9783655
Parshuram Kumar <learnerparshuram@gmail.com>
2026-05-23 12:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (99.88%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) {
         //brute approach : 
         // n ^ 2 for each element there will be a loop to find the target  - curelement
 
         //map  - n but extra space
-        map<int, int> mp;
+        // map<int, int> mp;
 
-        for(int i = 0; i < numbers.size(); ++i){
-            int curElement = numbers[i];
-            int toFind = target - curElement;
+        // for(int i = 0; i < numbers.size(); ++i){
+        //     int curElement = numbers[i];
+        //     int toFind = target - curElement;
 
-            if(mp.contains(toFind)){
-                return {mp[toFind] + 1, i +1};
+        //     if(mp.contains(toFind)){
+        //         return {mp[toFind] + 1, i +1};
+        //     }
+
+        //     mp[curElement] = i;
+        // }
+        // return {-1, -1};
+
+        //with constant space but the trade off will be nlogn
+        int left = 0, right = nums.size() - 1;
+
+        while(left < right){
+            if(nums[left] + nums[right] == target){
+                return {left + 1, right + 1};
+            }else if(nums[left] + nums[right] < target){
+                left++;
+            }else{
+                right--;
             }
 
-            mp[curElement] = i;
         }
+
         return {-1, -1};
     }
 };
